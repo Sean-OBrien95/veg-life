6c8a71441c4dd0894ec8426d4288d1c7b896ecc4
Sean-OBrien95 <>
2023-12-04 21:13:38 +0000

    Add views for posts and placed basic html stucture for posts

--- posts/views.py
+++ posts/views.py
@@ -4,8 +4,16 @@ from django.contrib.auth import login, forms
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm, UserProfileForm
-from .models import UserProfile
+from .models import UserProfile, Post
 from django.db import models
+from django.views import generic
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
 
 
 @login_required
