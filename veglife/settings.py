5cfb6f39f474001a2dab3550cd69385190e5926c
Seán O'Brien <sean95ob@gmail.com>
2024-02-03 14:47:42 +0000

    Turned debug off, preparing for deployment

--- veglife/settings.py
+++ veglife/settings.py
@@ -29,10 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '8000-sean-obrien95-veg-life-wyjt3j0dvl.us2.codeanyapp.com', 'https://veg-life-e7290059d7ac.herokuapp.com/', '8000-seanobrien95-veglife-3r5ds5ppvii.ws-eu107.gitpod.io']
+    '8000-sean-obrien95-veg-life-wyjt3j0dvl.us2.codeanyapp.com', 'https://veg-life-e7290059d7ac.herokuapp.com/', '8000-seanobrien95-veglife-3r5ds5ppvii.ws-eu108.gitpod.io']
 
 
 # Application definition
